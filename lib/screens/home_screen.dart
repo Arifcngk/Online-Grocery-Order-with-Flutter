856496d5ef8ca816c747f0270a5a96aecd4a6d41
Arifcngk <93188117+Arifcngk@users.noreply.github.com>
2024-06-10 22:25:44 +0300

    feat: swiper bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:manav_sepeti/provider/dark_theme_provider.dart';
+import 'package:manav_sepeti/services/utils.dart';
 import 'package:provider/provider.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,22 +12,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> _slider = [
+    'images/slider1.jpg',
+    'images/slider2.jpg',
+    'images/slider3.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
-    final themeState = Provider.of<DarkThemeProvider>(context);
+    final Utils utils = Utils(context);
+    final themeState = utils.getTheme;
+    Size size = utils.getScreenSize;
     return Scaffold(
-      body: Center(
-        child: SwitchListTile(
-          title: Text('Dark Mode'),
-          secondary: Icon(themeState.getDarkTheme
-              ? Icons.dark_mode_outlined
-              : Icons.light_mode_outlined),
-          onChanged: (bool value) {
-            setState(() {
-              themeState.setDarkTheme = value;
-            });
+      body: SizedBox(
+        height: size.height * 0.3,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.asset(
+              _slider[index],
+              fit: BoxFit.cover,
+            );
           },
-          value: themeState.getDarkTheme,
+          itemCount: _slider.length,
+          pagination: const SwiperPagination(
+            builder: DotSwiperPaginationBuilder(
+              color: Colors.grey,
+              activeColor: Colors.orange,
+            ),
+            alignment: Alignment.bottomCenter,
+          ),
+          autoplay: true,
+          // control: const SwiperControl(color: Colors.orangeAccent),
         ),
       ),
     );
